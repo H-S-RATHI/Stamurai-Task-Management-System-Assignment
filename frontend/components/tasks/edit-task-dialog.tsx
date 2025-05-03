3f1535214a19c6354e8c5daeec2c59c8ed225a2d
H-S-RATHI <goluu.000111@gmail.com>
2025-05-03 23:48:23 +0530

    Fix default value assignment in EditTaskDialog: Update the Select component to use field.value instead of field.assigneeId for better state management.

--- frontend/components/tasks/edit-task-dialog.tsx
+++ frontend/components/tasks/edit-task-dialog.tsx
@@ -232,7 +232,7 @@ export function EditTaskDialog({ task, users, open, onOpenChange }: EditTaskDial
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Assignee</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.assigneeId}>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue placeholder="Assign to..." />
