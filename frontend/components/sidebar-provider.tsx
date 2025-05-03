af26f2b782491f448cc6185dbb03078042d5920a
H-S-RATHI <goluu.000111@gmail.com>
2025-05-03 10:36:41 +0530

    side bar added

---
+++ frontend/components/sidebar-provider.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import type React from "react"
+
+import { SidebarProvider as Provider } from "@/components/ui/sidebar"
+
+export function SidebarProvider({ children }: { children: React.ReactNode }) {
+  return <Provider>{children}</Provider>
+}
